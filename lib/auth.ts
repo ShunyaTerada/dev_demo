27b40e4321b1c65fcfd9f4de2109d70363a1758a
kibaku <shunyaterada@gmail.com>
2025-11-11 11:55:37 +0900

    Initial commit: Setup Next.js with Better Auth, Drizzle ORM, and Supabase

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import "server-only";
+import { betterAuth } from "better-auth";
+import { nanoid } from "nanoid";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+import { db } from "../db";
+import { getBaseURL } from "./get-base-url";
+import * as schema from "../db/schema/auth";
+
+export const auth = betterAuth({
+  baseURL: getBaseURL(),
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    schema
+  }),
+  advanced: {
+    generateId: () => nanoid(10),
+  },
+  plugins: [nextCookies()],
+});
